fc89c7df454a82dc6440806f17930c2139d15091
Administrator <Administrator@jeongyuejin>
2018-05-01 11:40:42 +0900

    고양이귀여웡 180501

---
+++ java/src/my/incdecexample1.java
@@ -0,0 +1,16 @@
+package my;
+
+public class incdecexample1 
+public static void main(String args[])
+{
+	int num1 = 1, num = 2 , num3 = 0, num4 = 1;
+	int rusult1 = num1++;
+	int rusult2 = num2--;
+	int rusult3 = ++num3;
+	int rusult4 = --num4;
+	System.out.println("rusult1 = + rusult1");
+	System.out.println("rusult2 = + rusult2");
+	System.out.println("rusult3 = + rusult3");
+	System.out.println("rusult4 = + rusult4");
+   }
+}
